60e8e75cc8ee7c88f8314864e965d178ada89b60
Harry Braviner <harry.braviner@gmail.com>
2022-01-09 20:48:21 -0500

    Writing small array succeeds

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 use std::path::Path;
 use std::fs::File;
-use std::io::{BufWriter, Write};
+use std::io::{BufWriter, Write, Seek};
 
 pub struct NPYStream {
     writer: BufWriter<File>,
@@ -10,14 +10,15 @@ pub struct NPYStream {
 
 impl NPYStream {
     pub fn new(path: &Path, num_cols: usize) -> Self {
-        let file = File::open(path).unwrap();   // FIXME - error handling
+        let file = File::create(path).unwrap();   // FIXME - error handling
         let mut writer = BufWriter::new(file);
 
         // Write the header to the file.
         // This will be overwritten when the struct is finally dropped, but writing this now
         // ensures that the correct number of bytes at the start of the file are reserved for the
         // header.
-        writer.write(&make_header(0, num_cols));
+        writer.write(&make_header(0, num_cols))
+            .unwrap_or_else(|err| panic!("Error writing header: {:?}", err));
 
         NPYStream {writer, num_cols, num_rows: 0}
     }
@@ -38,8 +39,11 @@ impl NPYStream {
 
 impl Drop for NPYStream {
     fn drop(&mut self) {
-        // FIXME - write number of rows to shape
-        unimplemented!()
+        self.writer.flush()
+            .and_then(|()| self.writer.rewind())
+            .and_then(|()| self.writer.write(&make_header(self.num_rows, self.num_cols)))
+            .and_then(|_num_bytes_written| self.writer.flush())
+            .unwrap_or_else(|err| panic!("Error closing numpy stream: {:?}", err));
     }
 }
 
@@ -74,11 +78,26 @@ fn make_header(num_rows: usize, num_cols: usize) -> Vec<u8> {
 #[cfg(test)]
 mod tests {
     use crate::make_header;
+    use tempfile::tempdir;
+    use super::*;
 
     #[test]
     fn header_len_correct() {
         assert_eq!(make_header(1, 1).len(), 128);
         assert_eq!(make_header(100, 100).len(), 128);
         assert_eq!(make_header(1_000_000, 100).len(), 128);
+    }
+
+    #[test]
+    fn small_write_succeeds() {
+        let dir = tempdir().unwrap();
+        let file_path = dir.path().join("small_array.npy");
+
+        let mut npy_stream : NPYStream = NPYStream::new(&file_path, 2);
+
+        npy_stream.write(vec![0.0, 1.0]);
+        npy_stream.write(vec![2.0, 3.0]);
+
+        drop(npy_stream);   // Cause the file to be closed
     }
 }
